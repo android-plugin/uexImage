60c9fbe2bf39bfee3e11c60c1f72d25a9e10324d
JingweiWang <13622032005@qq.com>
2016-03-03 15:43:06 +0800

    fix typo

--- src/org/zywx/wbpalmstar/plugin/ueximage/AlbumListActivity.java
+++ src/org/zywx/wbpalmstar/plugin/ueximage/AlbumListActivity.java
@@ -54,8 +54,8 @@ import java.util.Comparator;
 import java.util.List;
 
 public class AlbumListActivity extends Activity implements Serializable {
-	private static final long serialVersionUID = 1L;
-	public static final String TAG = "AlbumListActivity";
+    private static final long serialVersionUID = 1L;
+    public static final String TAG = "AlbumListActivity";
     private ListView lvAlbumList;
 
     private UEXImageUtil uexImageUtil;
