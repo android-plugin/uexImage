196c694131201c0ec487920e127d48ceaac20e11
fred_personal <fred.yeah@outlook.com>
2016-12-06 16:05:47 +0800

    新增添加标签相关的接口

--- src/org/zywx/wbpalmstar/plugin/ueximage/EUExImage.java
+++ src/org/zywx/wbpalmstar/plugin/ueximage/EUExImage.java
@@ -21,30 +21,44 @@ package org.zywx.wbpalmstar.plugin.ueximage;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Environment;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.zywx.wbpalmstar.base.ACEImageLoader;
 import org.zywx.wbpalmstar.base.BDebug;
 import org.zywx.wbpalmstar.base.BUtility;
 import org.zywx.wbpalmstar.base.ResoureFinder;
+import org.zywx.wbpalmstar.engine.DataHelper;
 import org.zywx.wbpalmstar.engine.EBrowserView;
 import org.zywx.wbpalmstar.engine.universalex.EUExBase;
 import org.zywx.wbpalmstar.plugin.ueximage.crop.Crop;
+import org.zywx.wbpalmstar.plugin.ueximage.model.LabelInfo;
 import org.zywx.wbpalmstar.plugin.ueximage.util.CommonUtil;
 import org.zywx.wbpalmstar.plugin.ueximage.util.Constants;
 import org.zywx.wbpalmstar.plugin.ueximage.util.EUEXImageConfig;
 import org.zywx.wbpalmstar.plugin.ueximage.util.UEXImageUtil;
+import org.zywx.wbpalmstar.plugin.ueximage.widget.LabelView;
+import org.zywx.wbpalmstar.plugin.ueximage.widget.LabelViewContainer;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 public class EUExImage extends EUExBase {
@@ -54,15 +68,12 @@ public class EUExImage extends EUExBase {
     private boolean cropUsePng = false;
     //当前Android只支持方型裁剪, 即cropMode为1
     //cropMode 为4: 矩型裁剪,比例为 4:3, cropMode为5: 矩形裁剪, 比例为16:9 cropMode 为6：自由缩放
-
     private int cropMode = 1;
-
-
     private File cropOutput = null;
 
     //裁剪操作的状态，1 代表成功， 2 代表失败，3代表用户取消。
     private int cropStatus = 1;
-
+    RelativeLayout labelViewContainer;
     public static final int REQUEST_CROP_IMAGE = 100;
     public static final int REQUEST_IMAGE_PICKER = 101;
     public static final int REQUEST_IMAGE_BROWSER = 102;
@@ -136,7 +147,6 @@ public class EUExImage extends EUExBase {
             EUEXImageConfig.getInstance().setIsOpenBrowser(false);
             Intent intent = new Intent(context, AlbumListActivity.class);
             startActivityForResult(intent, REQUEST_IMAGE_PICKER);
-
         } catch (JSONException e) {
             if (BDebug.DEBUG) {
                 Log.i(TAG, e.getMessage());
@@ -212,7 +222,6 @@ public class EUExImage extends EUExBase {
             }
             //Android不支持
             //boolean isDisplayNavArrows = jsonObject.getBoolean("displayNavArrows");
-
             if (jsonObject.has("startIndex")) {
                 int startIndex = jsonObject.getInt("startIndex");
                 if (startIndex < 0) {
@@ -233,6 +242,160 @@ public class EUExImage extends EUExBase {
         } catch (JSONException e) {
             e.printStackTrace();
             Toast.makeText(context, "JSON解析错误", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //打开一个可以添加Label的背景图片， 所有的Label都添加在这个图片上
+    public void openLabelViewContainer(String [] params) {
+        String json = params[0];
+        try {
+            JSONObject obj = new JSONObject(json);
+            int w = obj.getInt("width");
+            int h = obj.getInt("height");
+            int x = obj.optInt("x", 0);
+            int y = obj.optInt("y", 0);
+            String imagePath = obj.getString("image");
+            labelViewContainer = new RelativeLayout(mContext);
+            Bitmap bitmap = ACEImageLoader.getInstance().getBitmapSync(imagePath);
+            BitmapDrawable drawable = new BitmapDrawable(bitmap);
+            labelViewContainer.setBackgroundDrawable(drawable);
+
+            RelativeLayout.LayoutParams param = new RelativeLayout.LayoutParams(w, h);
+            param.leftMargin = x;
+            param.topMargin = y;
+            addViewToCurrentWindow(labelViewContainer, param);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    //添加label
+    public void addLabelView(String [] params) {
+        if (labelViewContainer == null) {
+            Log.i(TAG, "please call openLabelView method first");
+            return;
+        }
+        if (params.length < 0) {
+            return;
+        }
+        try {
+            //如果传过来的是Json对象，代表添加一个label.
+            if (params[0].startsWith("{")) {
+                JSONObject obj = new JSONObject(params[0]);
+                addLabel(obj);
+            } else {
+                JSONArray array = new JSONArray(params[0]);
+                int len = array.length();
+                for (int i = 0; i < len; i++) {
+                    JSONObject obj = array.getJSONObject(i);
+                    addLabel(obj);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addLabel(JSONObject obj) throws JSONException {
+        String id = obj.getString("id");
+        String content = obj.getString("content");
+        if (!TextUtils.isEmpty(id) && !TextUtils.isEmpty(content)) {
+            int x = obj.optInt("x", 0);
+            int y = obj.optInt("y", 0);
+            LabelView labelView = new LabelView(mContext, labelViewContainer, id, content);
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.leftMargin = x;
+            layoutParams.topMargin = y;
+            labelViewContainer.addView(labelView, layoutParams);
+        }
+    }
+
+    /**
+     * 获取图片及label信息，数据格式如下:
+     * {
+         "width": 600, // 外层图片（label 的载体）的宽度
+         "height": 333,  // 外层图片的高度
+         "labels": [     //所有的label
+             {
+             "id": "1",  //label id, 当用户点击图片时， 若点到了label所标识的热点，会触发onLabelClicked方法，该方法会返回id
+             "content": "content", //label内容
+             "left": "0.033", //label左侧距其外层图片左侧的距离和图片宽度的比值。
+             "right": "0.342", //label右侧距其外层图片左侧的距离和图片宽度的比值。
+             "top": "0.120", //label右侧距其外层图片顶部的距离和图片高度的比值。
+             "bottom": "0.276", //label底部距其外层图片顶部的距离和图片高度的比值。
+             "targetPointMode": 0  //label所指向的热点相对label的位置，0： 在label左侧， 1: 在label右侧
+             }
+         ]
+      }
+     *
+     */
+    public JSONObject getPicInfoWithLabelViews(String [] params) {
+        if (labelViewContainer == null) {
+            Log.i(TAG, "please call openLabelView method first");
+            return null;
+        }
+        int childCount = labelViewContainer.getChildCount();
+        int width = labelViewContainer.getWidth();
+        int height = labelViewContainer.getHeight();
+        JSONObject result = new JSONObject();
+
+        try {
+            result.put("width", width);
+            result.put("height", height);
+
+            JSONArray array = new JSONArray();
+            for (int i = 0; i < childCount; i++) {
+                LabelView view = (LabelView) labelViewContainer.getChildAt(i);
+                String id = view.getLableId();
+                String content = view.getContent();
+                float left = view.getLeft() * 1.0f / labelViewContainer.getWidth();
+                float right = view.getRight() * 1.0f / labelViewContainer.getWidth();
+                float top = view.getTop() * 1.0f / labelViewContainer.getHeight();
+                float bottom = view.getBottom() * 1.0f / labelViewContainer.getHeight();
+                JSONObject obj = new JSONObject();
+                DecimalFormat format = new DecimalFormat("##0.000");
+
+                obj.put("id", id);
+                obj.put("content", content);
+                obj.put("left", format.format(left));
+                obj.put("right", format.format(right));
+                obj.put("top", format.format(top));
+                obj.put("bottom", format.format(bottom));
+                obj.put("targetPointMode", view.getTargetPointMode());
+                array.put(obj);
+            }
+            result.put("labels", array);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return result;
+
+    }
+
+    public void showLabelViewPic(String params[]) {
+        String json = params[0];
+        try {
+            JSONObject obj = new JSONObject(json);
+            int w = obj.getInt("width");
+            int h = obj.getInt("height");
+            int x = obj.optInt("x", 0);
+            int y = obj.optInt("y", 0);
+            String imagePath = obj.getString("image");
+
+            String labels = obj.getString("labels");
+            List<LabelInfo> infoList = DataHelper.gson.fromJson(labels, new TypeToken<ArrayList<LabelInfo>>(){}.getType());
+            LabelViewContainer container =  new LabelViewContainer(mContext, infoList);
+            Bitmap bitmap = ACEImageLoader.getInstance().getBitmapSync(imagePath);
+            BitmapDrawable drawable = new BitmapDrawable(bitmap);
+            container.setBackgroundDrawable(drawable);
+
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(w, h);
+            layoutParams.leftMargin = x;
+            layoutParams.rightMargin = y;
+            container.setUexBaseObj(this);
+            addViewToCurrentWindow(container, layoutParams);
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 
@@ -552,7 +715,7 @@ public class EUExImage extends EUExBase {
         return true;
     }
 
-    private void callBackPluginJs(String methodName, String jsonData){
+    public void callBackPluginJs(String methodName, String jsonData){
         String js = SCRIPT_HEADER + "if(" + methodName + "){"
                 + methodName + "('" + jsonData + "');}";
         onCallback(js);
